75883d11b947b7ecade8eeedd7b3056be4ae86d8
swoody2527 <swoodall2527@gmail.com>
2024-04-03 16:15:40 +0100

    unsaved work

--- scraper.py
+++ scraper.py
@@ -1,3 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+def scrape_website(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+    else:
+        print("Couldn't access data", response.status_code)
+        return None
+    
+
+page_data = scrape_website("https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Korean_5800")
+
+print(page_data)
